212a945e07b1db4310e9521ee4157da12ca82875
Kokulapalan Wimalanathan <kokul@iastate.edu>
2018-06-06 19:36:12 -0500

    starting to change the docker file

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
 docker build -t wkpalan/shiny-var . $@
+wget https://osf.io/4f7j2/download -O data.tar && tar -xvf data.tar
--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
-docker run -d -p 3838:80 wkpalan/shiny-var
+docker run -d \
+	-p 3838:80 \
+	--mount type=bind,source="$(pwd)"/data,target=/srv/shiny-server/shiny-var/data \
+	wkpalan/shiny-var
 #docker run -it -p 3838:80 wkpalan/shiny-var bash
